fb90908f999045209e642a59afc42d18dfb4d1a0
izasoerya <ihza.surya@gmail.com>
2025-08-27 00:21:41 +0700

    feat: inline method for better readibility

--- include/enum.h
+++ include/enum.h
@@ -12,3 +12,35 @@ enum ModbusCommandType
     HOLDING_REGISTER,
     INPUT_REGISTER,
 };
+
+inline const char *sensorTypeToString(SensorType type)
+{
+    switch (type)
+    {
+    case TEMPERATURE:
+        return "TEMPERATURE";
+    case HUMIDITY:
+        return "HUMIDITY";
+    case AMMONIA:
+        return "AMMONIA";
+    case LIGHT:
+        return "LIGHT";
+    case ANEMOMETER:
+        return "ANEMOMETER";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+inline const char *modbusCommandTypeToString(ModbusCommandType type)
+{
+    switch (type)
+    {
+    case HOLDING_REGISTER:
+        return "HOLDING_REGISTER";
+    case INPUT_REGISTER:
+        return "INPUT_REGISTER";
+    default:
+        return "UNKNOWN";
+    }
+}
